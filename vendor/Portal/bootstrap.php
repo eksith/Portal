7fc5e2d2211f04c09aef92559e76d29bcfcfa943
Pep҉p̡er G̸en҉e̸tik <eksith@live.com>
2016-04-09 22:44:25 -0400

    Update bootstrap.php

--- vendor/Portal/bootstrap.php
+++ vendor/Portal/bootstrap.php
@@ -1,3 +1,16 @@
 <?php
 
 # TODO
+
+define( 'PKGS',			PATH . 'vendor/' );
+
+# Use the |PATH| placeholder if the relevant folder/resource 
+# is relative to this config file. It's recommended that the databases 
+# and upload directories be placed outside the web root
+define( 'CONFIG',		PATH . 'data/blog.conf' );
+
+\set_include_path( \get_include_path() . PATH_SEPARATOR . PKGS );
+\spl_autoload_extensions( '.php' );
+\spl_autoload_register( function( $class ) {
+	\spl_autoload( str_replace( "\\", "/", $class ) );
+});
